d9b93dbc5594ae1efa424e88658bf52fd23c6578
MayC007 <mathcollart@gmail.com>
2025-07-13 22:06:55 +0200

    serveur.js

--- serveur.js
+++ serveur.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+// Sert le dossier "public" pour les fichiers HTML/CSS/JS
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 const port = process.env.PORT || 3000;
