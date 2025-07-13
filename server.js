89cf9e81e35abb4730ab21fc3038df537de977c8
MayC007 <mathcollart@gmail.com>
2025-07-13 21:34:27 +0200

    serveur.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-// server.js - TRONQUÉ POUR L'EXEMPLE
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serveur actif sur le port ${port}`);
+});
