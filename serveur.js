70f429bc95762a58a32b1bf1bf0c63578efce180
MayC007 <mathcollart@gmail.com>
2025-07-14 21:16:05 +0200

    fix: serveur gère joueur et état du plateau

--- serveur.js
+++ serveur.js
@@ -1,87 +1,47 @@
 const express = require("express");
-const path = require("path");
-const http = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-const port = process.env.PORT || 3000;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-let rooms = {};
+let players = {};
+let boardState = [];
 
 function initializeBoard() {
-  const cells = Array(64).fill(null);
-  cells[27] = "mayc";
-  cells[28] = "mone";
-  cells[35] = "mone";
-  cells[36] = "mayc";
-  return cells;
+  boardState = Array(8).fill(null).map(() => Array(8).fill(""));
+  boardState[3][3] = "mayc";
+  boardState[3][4] = "mone";
+  boardState[4][3] = "mone";
+  boardState[4][4] = "mayc";
 }
 
+initializeBoard();
+
 io.on("connection", (socket) => {
-  socket.on("join", ({ room, player }) => {
-    if (!rooms[room]) {
-      rooms[room] = {
-        players: [],
-        cells: initializeBoard(),
-      };
-    }
+  console.log("Nouvelle connexion :", socket.id);
 
-    if (!rooms[room].players.includes(player)) {
-      if (rooms[room].players.length >= 2) {
-        socket.emit("full");
-        return;
-      }
-      rooms[room].players.push(player);
-    }
-
-    socket.join(room);
-    socket.room = room;
-    socket.player = player;
-
-    socket.emit("joined", {
-      player,
-      room,
-      cells: rooms[room].cells,
-    });
+  socket.on("join", (username) => {
+    players[socket.id] = username;
+    socket.emit("playerColor", username);
+    socket.emit("boardUpdate", boardState);
+    console.log(username + " a rejoint la partie.");
   });
 
-  socket.on("move", ({ room, updatedCells, nextPlayer }) => {
-    if (rooms[room]) {
-      rooms[room].cells = updatedCells;
-      io.to(room).emit("move", {
-        updatedCells,
-        nextPlayer,
-      });
-    }
-  });
-
-  socket.on("restart", (room) => {
-    if (rooms[room]) {
-      rooms[room].cells = initializeBoard();
-      io.to(room).emit("restart", rooms[room].cells);
+  socket.on("play", ({ x, y, player }) => {
+    if (boardState[y][x] === "") {
+      boardState[y][x] = player;
+      io.emit("boardUpdate", boardState);
     }
   });
 
   socket.on("disconnect", () => {
-    const room = socket.room;
-    const player = socket.player;
-    if (room && rooms[room]) {
-      rooms[room].players = rooms[room].players.filter((p) => p !== player);
-      if (rooms[room].players.length === 0) {
-        delete rooms[room];
-      }
-    }
+    console.log("Déconnexion :", socket.id);
+    delete players[socket.id];
   });
 });
 
-server.listen(port, () => {
-  console.log(`Serveur actif sur le port ${port}`);
+http.listen(3000, () => {
+  console.log("Serveur lancé sur http://localhost:3000");
 });
